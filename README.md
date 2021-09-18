c6977a56dffe78cd9aaf4889a1ac85d81e14b447
snsk <shinsk@gmail.com>
2021-09-18 17:08:06 +0900

    output numpy array

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ai_testing_neural_coverage
+
+自作の学習モデルに対するニューロンカバレッジ取得の実験
+
+* kerasのKfunctionで中間層の出力を全部吐く
+    * np.set_printoptions(threshold=sys.maxsize) 指定でnumpyの出力が省略されずに全部出る
+* 吐き出された配列要素のうち、0 でない要素をニューロン発火と考える
+* 配列要素数を分母として、0でない要素を分子とすると発火のcoverageが取れる？
--- metamorphic_testing_onefile.py
+++ metamorphic_testing_onefile.py
@@ -3,12 +3,18 @@ import matplotlib.pyplot as plt
 from keras.preprocessing import image
 from keras.models import load_model
 from keras.preprocessing.image import img_to_array, load_img
-import pprint
+from keras import backend as K
+import codecs
+import sys
 
-colab_root = '/content/drive/MyDrive/colab_root/'
+np.set_printoptions(threshold=sys.maxsize)
+
+#colab_root = '/content/drive/MyDrive/colab_root/'
+colab_root = './'
+filename = 'ramen_1.jpg'
 
 # 画像を読み込む。
-img = image.load_img(colab_root+'ramen_1.jpg',  target_size=(100,100))
+img = image.load_img(colab_root+filename,  target_size=(100,100))
 img = np.array(img)
 
 #plt.imshow(img)
@@ -30,6 +36,7 @@ g_img = g_img[None, ...]
 model = load_model(colab_root + 'ramen_hiyashi_acc0.9675.h5')
 # 正解ラベルの定義
 label=['ramen', 'hiyashi']
+model.summary()
 
 # 判別
 pred = model.predict(g_img, batch_size=1, verbose=0)
@@ -37,3 +44,16 @@ score = np.max(pred)
 pred_label = label[np.argmax(pred[0])]
 print('name:',pred_label)
 print('score:',score)
+
+layer_name=['cond2d_28', 'conv2d_29', 'maxpooling', 'dropout',
+'conv2d_30', 'conv2d31', 'maxpooling', 'dropout', 
+'flatten7', 'dence', 'dropout', 'dense(softmax)']
+
+# with a Sequential model
+for i in range(12):
+    get_3rd_layer_output = K.function([model.layers[0].input],
+                                    [model.layers[i].output])
+    layer_output = get_3rd_layer_output(g_img)[0]
+    print(layer_output, file=codecs.open(filename + '_numpy_l' + 
+        str(i) + '_' + layer_name[i] + '.txt', 'w', 'utf-8'))
+
